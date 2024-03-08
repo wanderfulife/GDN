0123f98badfa9519ffd7ed904089d82d6fe29ad2
wanderfulife <john.wander@hotmail.fr>
2024-03-08 09:51:58 +0100

    excel

--- src/components/reponses.js
+++ src/components/reponses.js
@@ -1,32 +1,32 @@
 const postes = [
 	{
 		id: 1,
-		output: "Poste 1",
+		output: "Paraf FR",
 		text: "Paraf France",
 	},
 	{
 		id: 2,
-		output: "Poste 2",
+		output: "Guichet FR",
 		text: "Guichet France",
 	},
 	{
 		id: 3,
-		output: "Poste 3",
+		output: "Paraf EN",
 		text: "Paraf Anglais",
 	},
 	{
 		id: 4,
-		output: "Poste 4",
+		output: "Guichet EN",
 		text: "Guichet Anglais",
 	},
 	{
 		id: 5,
-		output: "Poste 5",
+		output: "Controle 1",
 		text: "Controle baggage 1",
 	},
 	{
 		id: 6,
-		output: "Poste 6",
+		output: "Controle 2",
 		text: "controle baggage 2",
 	},
 ];
